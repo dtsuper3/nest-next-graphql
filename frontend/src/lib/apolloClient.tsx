b0d2a22a9be7139d3c3af8461beda9743661fbb0
Deepak Thapa <32347879+dtsuper3@users.noreply.github.com>
2022-12-06 23:42:14 +0530

    frontend tailwind css added, backend port change, docker-compose file added

--- frontend/src/lib/apolloClient.tsx
+++ frontend/src/lib/apolloClient.tsx
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
 const apolloClient = new ApolloClient({
-    uri: 'http://localhost:3000',
+    uri: 'http://localhost:5000',
     cache: new InMemoryCache(),
 });
 
